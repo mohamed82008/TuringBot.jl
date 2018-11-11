7bc15f6ba9515fbf028ca8893a7187a7428a9046
mohamed82008 <mohamed82008@gmail.com>
2018-11-11 15:12:29 +1100

    some fixes

--- src/TuringBot.jl
+++ src/TuringBot.jl
@@ -3,45 +3,54 @@ module TuringBot
 using GitHub, HTTP, Sockets
 
 # Authentication
-username = ENV["GITHUB_USERNAME"]
-myauth = GitHub.authenticate(ENV["GITHUB_AUTH"])
+const username = ENV["GITHUB_USERNAME"]
+const authtoken = ENV["GITHUB_AUTH"]
+const auth = GitHub.authenticate(authtoken)
 
-repo_name = "TuringLang/Turing.jl"
-myrepos = [GitHub.Repo(repo_name)] # can be Repos or repo names
-repo = myrepos[1]
+const sourcerepo_name = "TuringLang/Turing.jl"
+const sinkrepo_name = "TuringLang/TuringBenchmarks.jl"
+
+const sourcerepo = GitHub.Repo(sourcerepo_name)
+const listenrepos = [sourcerepo] # can be Repos or repo names
 
 snip(str, len) = str[1:min(len, end)]
 snipsha(sha) = snip(sha, 7)
+drop2(x) = x[3:end]
+repo_url(repo_name) = "https://$username:$(authtoken)@github.com/$(repo_name).git"
 
-repo_url(repo_name) = "https://$username:$(ENV["GITHUB_AUTH"])@github.com/$(repo_name).git"
-gitclone!(repo, path) = run(`git clone $(repo_url(repo)) $(path)`)
-function gitreset!(repo)
-    run(`git fetch $(repo_url(repo))`)
+gitclone!(path, repo_name) = run(`git clone $(repo_url(repo_name)) $(path)`)
+
+function gitreset!(repo_name)
+    run(`git fetch $(repo_url(repo_name))`)
     run(`git reset --hard origin/master`)
 end
+gitreset!(path, repo_name) = cd(()->gitreset!(repo_name), path)
 
-gitreset!(path, repo) = cd(()->gitreset!(repo), path)
-gitbranches(o="-l") = readlines(`git branch $o`) .|> (x) -> (x[3:end])
+function gitbranches(o="-l")
+    drop2.(readlines(`git branch $o`))
+end
 gitbranches(path, o) = cd(()->gitbranches(o), path)
+
 function gitcheckout!(branch)
     branch_exists = (branch in gitbranches())
     branch_exists ? run(`git checkout $branch`) : run(`git checkout -b $branch`)
     branch_exists
 end
-
 gitcheckout!(path, branch) = cd(()->gitcheckout!(branch), path)
+
 gitadd!() = run(`git add .`)
 gitadd!(path) = cd(gitadd!, path)
-gitcommit!(;message) = run(`git commit -m $message`)
-gitcommit!(path; message="") = cd(()->gitcommit!(message=message), path)
 
-function gitpush!(repo, branch)
-    run(`git push $(repo_url(repo)) $branch`)
+gitcommit!(; message=" ") = run(`git commit -m $message`)
+gitcommit!(path; message) = cd(()->gitcommit!(message=message), path)
+
+function gitpush!(repo_name, branch)
+    run(`git push -f $(repo_url(repo_name)) $branch`)
 end
-gitpush!(path, repo, branch) = cd(()->gitpush!(repo, branch), path)
+gitpush!(path, repo_name, branch) = cd(()->gitpush!(repo_name, branch), path)
 
-function find_pr(repo, base, head)
-    prs = GitHub.pull_requests(repo, auth=myauth)[1]
+function find_pr(repo::GitHub.Repo, base, head)
+    prs = GitHub.pull_requests(repo, auth=auth)[1]
     for i in 1:length(prs)
         pr = prs[i]
         if pr.head.label == head && pr.base.label == base
@@ -51,16 +60,14 @@ function find_pr(repo, base, head)
     return 0
 end
 
-sink_repo_name = "TuringBenchmarks"
-sink_repo = "$username/$repo_name"
 function update_remote(shas)
-    temp_dir = ".temp_$(sink_repo_name)"
+    temp_dir = ".temp_" * splitdir(sinkrepo_name)[2]
     if isdir(temp_dir)
-        gitreset!(temp_dir, sink_repo)
+        gitreset!(temp_dir, sinkrepo_name)
     else
-        gitclone!(temp_dir, sink_repo)
+        gitclone!(temp_dir, sinkrepo_name)
     end
-    error_msg = Ref("")
+    error_msg = Ref(" ")
     branch_name = join(snipsha.(shas), "_")
     make_pr = Ref(false)
     errored = Ref(false)
@@ -68,35 +75,54 @@ function update_remote(shas)
     try
         gitcheckout!(temp_dir, branch_name)
     catch err
-        error_msg[] = err.msg
+        if :msg ∈ fieldnames(typeof(err))
+            error_msg[] = err.msg
+        else
+            error_msg[] = "Error"
+        end
         errored[] = true
     end
-    cd(joinpath(temp_dir, "src")) do
-        try
-            filename = "bench_shas.txt"
-            if !("origin/$branch_name" in gitbranches("..", "-r"))
-                write("bench_shas.txt", join(shas, "\n"))
-                make_pr[] = true
-            end
-        catch err
-            error_msg[] = err.msg
-            errored[] = true
+    currentdir = pwd()
+    
+    srcdir = joinpath(temp_dir, "src")
+    isdir(srcdir) || mkdir(srcdir)
+    cd(srcdir)
+    try
+        filename = "bench_shas.txt"
+        if !("origin/$branch_name" in gitbranches("..", "-r"))
+            write("bench_shas.txt", join(shas, "\n"))
+            make_pr[] = true
         end
+    catch err
+        if :msg ∈ fieldnames(typeof(err))
+            error_msg[] = err.msg
+        else
+            error_msg[] = "Error"
+        end
+        errored[] = true
     end
+    cd(currentdir)
     if make_pr[]
         try
             gitadd!(temp_dir)
             gitcommit!(temp_dir; message="Update benchmarking shas")
-            gitpush!(temp_dir, sink_repo, branch_name)
+            try
+                gitpush!(temp_dir, sinkrepo_name, branch_name)
+            catch
+                throw("Push failed.")
+            end
             #rm(temp_path, recursive=true)
         catch err
-            error_msg[] = err.msg
+            if :msg ∈ fieldnames(typeof(err))
+                error_msg[] = err.msg
+            else
+                error_msg[] = "Error"
+            end
             errored[] = true
             make_pr[] = false
         end
     end
-    gitreset!(temp_dir, sink_repo)
-    #run(`rm -r $temp_dir`)
+    gitreset!(temp_dir, sinkrepo_name)
     return errored[], error_msg[], make_pr[], branch_name
 end
 
@@ -109,20 +135,21 @@ maintainers = ["xukai92",
                "willtebbutt",
                "wesselb"]
 
-myevents = ["issue_comment"]
-listener = GitHub.EventListener(auth = myauth,
-                                repos = myrepos,
-                                events = myevents) do event
+events = ["issue_comment"]
+listener = GitHub.EventListener(auth = auth,
+                                repos = listenrepos,
+                                events = events) do event
     kind, payload, repo = event.kind, event.payload, event.repository
     base_sha = GitHub.branch(repo, "master").commit.sha
 
     if kind == "issue_comment" && (payload["comment"]["user"]["login"] in maintainers) && 
         occursin("`@TuringBot benchmark`", payload["comment"]["body"]) 
+        
         if !haskey(event.payload["issue"], "pull_request")
-            body = "Benchmarking jobs cannot be triggered from issue comments (only PRs or commits)"
+            body = "Benchmarking jobs cannot be triggered from issue comments (only PRs or commits)."
             params = Dict("body"=>body)
             issue = GitHub.issue(repo, payload["issue"]["number"])
-            GitHub.create_comment(repo, issue, :issue, params=params, auth=myauth)    
+            GitHub.create_comment(repo, issue, :issue, params=params, auth=auth)    
             return HTTP.Response(400)
         end
         
@@ -130,7 +157,7 @@ listener = GitHub.EventListener(auth = myauth,
         if event.payload["action"] != "created"
             body = "No action taken (submission was from an edit or delete)."
             params = Dict("body"=>body)
-            GitHub.create_comment(repo, pr, :pr, params=params, auth=myauth)
+            GitHub.create_comment(repo, pr, :pr, params=params, auth=auth)
             return HTTP.Response(204)
         end
         
@@ -149,15 +176,19 @@ listener = GitHub.EventListener(auth = myauth,
             params = Dict("title" => "Benchmarking $(snipsha(sha)) against $(snipsha(base_sha))", 
                 "head" => branch_name, "base" => "master", "body" => body, "maintainer_can_modify" => true)
             try
-                i = find_pr(Repo(sink_repo), params["base"], params["head"])
+                i = find_pr(Repo(sinkrepo_name), params["base"], params["head"])
                 if i == 0
-                    new_pr = GitHub.create_pull_request(Repo(sink_repo); auth=myauth, params=params)
+                    new_pr = GitHub.create_pull_request(Repo(sinkrepo_name); auth=auth, params=params)
                 else
-                    new_pr = GitHub.pull_requests(repo, auth=myauth)[1][i]
+                    new_pr = GitHub.pull_requests(repo, auth=auth)[1][i]
                 end
                 body = "A benchmarking job has been scheduled in $(new_pr.html_url.uri)."
             catch err
-                error_msg = err.msg
+                if :msg ∈ fieldnames(typeof(err))
+                    error_msg = err.msg
+                else
+                    error_msg = "Error"
+                end
                 body *= "I could not schedule a benchmarking job."
                 if error_msg != ""
                     body *= "\n\nError: \n ```julia \n $error_msg \n ```"
@@ -169,7 +200,7 @@ listener = GitHub.EventListener(auth = myauth,
             body *= "Commit $(snipsha(sha)) and commit $(snipsha(base_sha)) have already been benchmarked before."
         end
         params = Dict("body"=>body)
-        GitHub.create_comment(repo, pr, :pr, params=params, auth=myauth)            
+        GitHub.create_comment(repo, pr, :pr, params=params, auth=auth)
     end
     return HTTP.Response(200)
 end
